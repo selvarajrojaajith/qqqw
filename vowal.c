2c64434746a48e7b12b6cc2bd9755e34e12eb58c
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-03-28 16:57:08 +0530

    vowal.c

---
+++ vowal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    char line[150];
+    int i, vowels, consonants, digits, spaces;
+
+    vowels =  consonants = digits = spaces = 0;
+
+    printf("Enter a line of string: ");
+    scanf("%[^\n]", line);
+
+    for(i=0; line[i]!='\0'; ++i)
+    {
+        if(line[i]=='a' || line[i]=='e' || line[i]=='i' ||
+    
+        {
+            ++vowels;
+        }
+        else if((line[i]>='a'&& line[i]<='z') || (line[i]>='A'&& line[i]<='Z'))
+        {
+            ++consonants;
+        }
+        else if(line[i]>='0' && line[i]<='9')
+        {
+            ++digits;
+            return 0
+           ;
+           }
